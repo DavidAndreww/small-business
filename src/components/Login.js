d998b5f838eafb045b1432a656751312e103807a
David Bille <d_bille@yahoo.com>
2020-02-18 08:44:50 -0600

    login page started

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography'
 
 const Login = () => {
   return (
-    <p>Login!</p>
+    <form>
+      <TextField variant="outlined" label="Username:" />
+      <br />
+      <TextField variant="outlined" label="Password" />
+      <br />
+      <Button color="primary" variant="contained" type="submit">
+        Login
+      </Button>
+      <Typography>- OR -</Typography>
+      <Button color="primary" variant="contained" type="submit">
+        Continue as Guest
+      </Button>
+    </form>
   )
 }
 
