df3486822dae216226924079e3a9b2f4891020e3
David Bille <d_bille@yahoo.com>
2020-05-16 00:35:24 -0500

    working on make protected routes

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -13,55 +13,21 @@ import { Link } from "react-router-dom";
 import { checkAuth } from '../HelperFunctions';
 
 const Listings = (props) => {
+
+preferredBehaviors = [
+
+]
+
+nonPreferredBehaviors = [
+
+]
   
     return (
-      <Container maxWidth="lg" className="listings">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <b>Name</b>
-              </TableCell>
-              <TableCell>
-                <b>Description</b>
-              </TableCell>
-              <TableCell>
-                <b>Hours</b>
-              </TableCell>
-              <TableCell>
-                <b>Address</b>
-              </TableCell>
-              {checkAuth() && (
-                <TableCell>
-                  <b>Delete</b>
-                </TableCell>
-              )}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.listings.map((obj, idx) => (
-              <TableRow key={obj.name}>
-                <TableCell>
-                  <Link to={`/details/${obj.name}`}>{obj.name}</Link>
-                </TableCell>
-                <TableCell>{obj.description}</TableCell>
-                <TableCell>{obj.hours}</TableCell>
-                <TableCell>{obj.address}</TableCell>
-                {checkAuth() && (
-                  <TableCell>
-                    <Button>
-                      <DeleteIcon
-                        onClick={() => props.deleteBiz(obj.name)}
-                        className="delete-icon"
-                      />
-                    </Button>
-                  </TableCell>
-                ) }
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Container>
+      <form action="" onSubmit={handlSubmit}>
+        {behaviorlist.map(behavior=>{
+          <input type="checkbox" value={behavior.value}>behavior.name</input>
+        })}
+      </form>
     );
   
 }
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,53 +1,61 @@
 const fetchCoords = (url) => {
   return (dispatch) => {
     fetch(url)
-    .then(res => res.json())
-    .then(response => {
-      const action = {
-        type: 'FETCH_COORDS',
-         value: response.results[0].geometry.location
-      }
-      dispatch(action)
-    })
-  }
-}
+      .then((res) => res.json())
+      .then((response) => {
+        const action = {
+          type: "FETCH_COORDS",
+          value: response.results[0].geometry.location,
+        };
+        dispatch(action);
+      });
+  };
+};
 
-const logOut = () =>{
+const logOut = () => {
   return {
-    type: 'LOG_OUT'
-  }
-}
+    type: "LOG_OUT",
+  };
+};
 
 const logIn = () => {
   return {
-    type: 'LOG_IN',
-  }
-}
+    type: "LOG_IN",
+  };
+};
 
 const logInGuest = () => {
   return {
-    type: 'LOG_IN_GUEST'
-  }
-}
+    type: "LOG_IN_GUEST",
+  };
+};
 
 const logOutGuest = () => {
   return {
-    type: 'LOG_OUT_GUEST'
-  }
-}
+    type: "LOG_OUT_GUEST",
+  };
+};
 
-const deleteBiz = (business) =>{
+const deleteBiz = (business) => {
   return {
-    type: 'DELETE_BIZ',
-    value: business
-  }
-}
+    type: "DELETE_BIZ",
+    value: business,
+  };
+};
 
 const addBiz = (business) => {
   return {
-    type: 'ADD_BIZ',
-    value: business
-  }
-}
+    type: "ADD_BIZ",
+    value: business,
+  };
+};
 
-export { fetchCoords, logIn, logOut, logInGuest, logOutGuest, deleteBiz, addBiz }
+export {
+  fetchCoords,
+  logIn,
+  logOut,
+  logInGuest,
+  logOutGuest,
+  deleteBiz,
+  addBiz,
+};
